6a8479522c93248596faeb76ab19b6809d669988
Brian Healey <brian.healey@digitalasset.com>
2026-09-08 21:01:12 -0400

    tag greatest version

--- upload-and-tag-dars.sh
+++ upload-and-tag-dars.sh
@@ -41,8 +41,8 @@ process_dar() {
   artifact_path="europe-docker.pkg.dev/da-images/playground/dars/${artifact}:${version}"
 
   dpm publish dar "oci://${artifact_path}" -f "$dar" \
-    --license ./splice-node/LICENSE \
-    --extra-tags "${tag}"
+    --license ./splice-node/LICENSE
+#    --extra-tags "${tag}"
 
   echo "Tagging $artifact version $version tag $tag"
   oras tag "$artifact_path" "$tag"
@@ -54,6 +54,34 @@ export ENVIRONMENT
 find "$DARS_DIR" -type f -name '*.dar' -print0 |
   # run in parallel as there are many files to process
   xargs -0 -n 1 -P 16 bash -c 'process_dar "$1"' _
+
+tag_greatest() {
+  # Find the highest version for each artifact and tag it latest
+  find "$DARS_DIR" -type f -name '*.dar' -print0 |
+    while IFS= read -r -d '' dar; do
+      file=${dar##*/}
+      if [[ $file =~ ^(.+)-([0-9]+\.[0-9]+\.[0-9]+)\.dar$ ]]; then
+        printf '%s\t%s\t%s\n' "${BASH_REMATCH[1]}" "${BASH_REMATCH[2]}" "$dar"
+      fi
+    done |
+    sort -t$'\t' -k1,1 -k2,2V |
+    awk -F'\t' '
+      $1 != prev {
+        if (prev != "") print last
+        prev = $1
+      }
+      { last = $0 }
+      END {
+        if (prev != "") print last
+      }
+    ' |
+    while IFS=$'\t' read -r name version dar; do
+      artifact_path="europe-docker.pkg.dev/da-images/playground/dars/${name}:${version}"
+      oras tag "$artifact_path" "$tag"
+    done
+}
+
+tag_greatest
 
 echo "Done."
 
